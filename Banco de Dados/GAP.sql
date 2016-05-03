ebbfa21fc1925e79371724f88b0e2ad10830487d
Felipe Muniz <mrmunz2012@gmail.com>
2016-05-03 20:03:24 -0300

    Criado o DB
    Criado o banco de dados em mysql utilizando o navicat data modeler

---
+++ Banco de Dados/GAP.sql
@@ -0,0 +1,38 @@
+CREATE TABLE `usuario` (
+`id` int(11) NOT NULL,
+`nome` varchar(255) NULL,
+`email` varchar(255) NULL,
+`login` varchar(255) NULL,
+`senha` varchar(255) NULL,
+PRIMARY KEY (`id`) 
+);
+
+CREATE TABLE `grupo_usuario` (
+`id` int(11) NOT NULL,
+`nome` varchar(255) NULL,
+PRIMARY KEY (`id`) 
+);
+
+CREATE TABLE `projeto` (
+`id` int(11) NOT NULL,
+`descricao` text NULL,
+`data_criacao` timestamp NULL ON UPDATE CURRENT_TIMESTAMP,
+`data_final` timestamp NULL ON UPDATE CURRENT_TIMESTAMP,
+PRIMARY KEY (`id`) 
+);
+
+CREATE TABLE `backlog` (
+`id` int(11) NOT NULL,
+`data_criacao` timestamp NULL ON UPDATE CURRENT_TIMESTAMP,
+`data_modificacao` timestamp NULL ON UPDATE CURRENT_TIMESTAMP,
+PRIMARY KEY (`id`) 
+);
+
+CREATE TABLE `userstory` (
+`id` int(11) NOT NULL,
+`descricao` text NULL,
+`peso` int(11) NULL,
+`prioridade` varchar(255) NULL,
+PRIMARY KEY (`id`) 
+);
+
