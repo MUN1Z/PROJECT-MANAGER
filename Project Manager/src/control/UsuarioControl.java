0336b978bbd5ca50875bccac86930654ba101a45
raiffsantos <raiff_santos@hotmail.com>
2016-05-03 22:22:59 -0300

    Criação da classe UsuarioControl da camada control

---
+++ Project Manager/src/control/UsuarioControl.java
@@ -0,0 +1,18 @@
+
+package control;
+
+/**
+ *
+ * @author Raiff
+ */
+public class UsuarioControl {
+    
+    public void cadastrarUsuario(){
+    
+    }
+    
+    public void deletarUsuario(){
+    
+    }
+    
+}
