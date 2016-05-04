48c18cb6954c83c4e59f8496d5b7687b8127a274
RaiffSantos <raiff_santos@hotmail.com>
2016-05-04 07:00:53 -0300

    Declarando id das tabelas como auto_increment

--- Banco de Dados/GAP.sql
+++ Banco de Dados/GAP.sql
@@ -1,38 +1,80 @@
-CREATE TABLE `usuario` (
-`id` int(11) NOT NULL,
-`nome` varchar(255) NULL,
-`email` varchar(255) NULL,
-`login` varchar(255) NULL,
-`senha` varchar(255) NULL,
-PRIMARY KEY (`id`) 
-);
-
-CREATE TABLE `grupo_usuario` (
-`id` int(11) NOT NULL,
-`nome` varchar(255) NULL,
-PRIMARY KEY (`id`) 
-);
-
-CREATE TABLE `projeto` (
-`id` int(11) NOT NULL,
-`descricao` text NULL,
-`data_criacao` timestamp NULL ON UPDATE CURRENT_TIMESTAMP,
-`data_final` timestamp NULL ON UPDATE CURRENT_TIMESTAMP,
-PRIMARY KEY (`id`) 
-);
-
-CREATE TABLE `backlog` (
-`id` int(11) NOT NULL,
-`data_criacao` timestamp NULL ON UPDATE CURRENT_TIMESTAMP,
-`data_modificacao` timestamp NULL ON UPDATE CURRENT_TIMESTAMP,
-PRIMARY KEY (`id`) 
-);
-
-CREATE TABLE `userstory` (
-`id` int(11) NOT NULL,
-`descricao` text NULL,
-`peso` int(11) NULL,
-`prioridade` varchar(255) NULL,
-PRIMARY KEY (`id`) 
-);
-
+CREATE SCHEMA gap;
+
+USE gap;
+
+CREATE TABLE `usuario` (
+
+`id` int(11) NOT NULL  AUTO_INCREMENT,
+
+`nome` varchar(255) NULL,
+
+`email` varchar(255) NULL,
+
+`login` varchar(255) NULL,
+
+`senha` varchar(255) NULL,
+
+PRIMARY KEY (`id`) 
+
+);
+
+
+
+CREATE TABLE `grupo_usuario` (
+
+`id` int(11) NOT NULL AUTO_INCREMENT,
+
+`nome` varchar(255) NULL,
+
+PRIMARY KEY (`id`) 
+
+);
+
+
+
+CREATE TABLE `projeto` (
+
+`id` int(11) NOT NULL AUTO_INCREMENT,
+
+`descricao` text NULL,
+
+`data_criacao` timestamp NULL ON UPDATE CURRENT_TIMESTAMP,
+
+`data_final` timestamp NULL ON UPDATE CURRENT_TIMESTAMP,
+
+PRIMARY KEY (`id`) 
+
+);
+
+
+
+CREATE TABLE `backlog` (
+
+`id` int(11) NOT NULL AUTO_INCREMENT,
+
+`data_criacao` timestamp NULL ON UPDATE CURRENT_TIMESTAMP,
+
+`data_modificacao` timestamp NULL ON UPDATE CURRENT_TIMESTAMP,
+
+PRIMARY KEY (`id`) 
+
+);
+
+
+
+CREATE TABLE `userstory` (
+
+`id` int(11) NOT NULL AUTO_INCREMENT,
+
+`descricao` text NULL,
+
+`peso` int(11) NULL,
+
+`prioridade` varchar(255) NULL,
+
+PRIMARY KEY (`id`) 
+
+);
+
+
+
