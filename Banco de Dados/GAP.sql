98a00ba127c4b7e0aa809051f52db45db083537a
Stanley Jony <1jony2jony@gmail.com>
2016-05-20 13:51:14 -0300

    Update GAP.sql
    Modificando o Formato de datas

--- Banco de Dados/GAP.sql
+++ Banco de Dados/GAP.sql
@@ -1,38 +1,76 @@
-CREATE TABLE `usuario` (
-`id` int(11) NOT NULL,
-`nome` varchar(255) NULL,
-`email` varchar(255) NULL,
-`login` varchar(255) NULL,
-`senha` varchar(255) NULL,
-PRIMARY KEY (`id`) 
-);
-
-CREATE TABLE `grupo_usuario` (
-`id` int(11) NOT NULL,
-`nome` varchar(255) NULL,
-PRIMARY KEY (`id`) 
-);
-
-CREATE TABLE `projeto` (
-`id` int(11) NOT NULL,
-`descricao` text NULL,
-`data_criacao` timestamp NULL ON UPDATE CURRENT_TIMESTAMP,
-`data_final` timestamp NULL ON UPDATE CURRENT_TIMESTAMP,
-PRIMARY KEY (`id`) 
-);
-
-CREATE TABLE `backlog` (
-`id` int(11) NOT NULL,
-`data_criacao` date NULL,
-`data_modificacao` date NULL,
-PRIMARY KEY (`id`) 
-);
-
-CREATE TABLE `userstory` (
-`id` int(11) NOT NULL,
-`descricao` text NULL,
-`peso` int(11) NULL,
-`prioridade` varchar(255) NULL,
-PRIMARY KEY (`id`) 
-);
-
+CREATE TABLE `usuario` (
+
+`id` int(11) NOT NULL auto_increment,
+
+`nome` varchar(255) NULL,
+
+`email` varchar(255) NULL,
+
+`login` varchar(255) NULL,
+
+`senha` varchar(255) NULL,
+
+PRIMARY KEY (`id`) 
+
+);
+
+
+
+CREATE TABLE `grupo_usuario` (
+
+`id` int(11) NOT NULL,
+
+`nome` varchar(255) NULL,
+
+PRIMARY KEY (`id`) 
+
+);
+
+
+
+CREATE TABLE `projeto` (
+
+`id` int(11) NOT NULL auto_increment,
+
+`descricao` text NULL,
+
+`data_criacao` varchar(10),
+
+`data_final` varchar(10),
+
+PRIMARY KEY (`id`) 
+
+);
+
+
+
+CREATE TABLE `backlog` (
+
+`id` int(11) NOT NULL,
+
+`data_criacao` varchar(10),
+
+`data_modificacao` varchar(10),
+
+PRIMARY KEY (`id`) 
+
+);
+
+
+
+CREATE TABLE `userstory` (
+
+`id` int(11) NOT NULL,
+
+`descricao` text NULL,
+
+`peso` int(11) NULL,
+
+`prioridade` varchar(255) NULL,
+
+PRIMARY KEY (`id`) 
+
+);
+
+
+
