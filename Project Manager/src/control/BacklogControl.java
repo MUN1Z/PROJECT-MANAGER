c589acf8b37f5152faf7730108c972258995bd47
Felipe Muniz <mrmunz2012@gmail.com>
2016-05-22 11:05:42 -0300

    Criado o metódo de cadastrar o BcklogControl

--- Project Manager/src/control/BacklogControl.java
+++ Project Manager/src/control/BacklogControl.java
@@ -1,6 +1,15 @@
 
 package control;
 
+import database.ConectaDB;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import model.Backlog;
+import model.Usuario;
+
 /**
  *
  * @author Raiff
@@ -18,8 +27,33 @@ public class BacklogControl {
         return controladorBacklog;
     }
     
-    public void cadastrarBacklog(){
-    
+    /**
+     * Método responsávem por cadastrar um backlog no banco de dados.
+     * O método recebe como parametro um objeto do tipo Usuario.
+     * @author Felipe Muniz
+     * @param backlog
+     * @return boolean
+     */
+    public static boolean cadastrarBacklog(Backlog backlog){
+        
+        ConectaDB conecta = new ConectaDB();
+        conecta.conexao();
+        
+        String sql = "INSERT INTO backlog() VALUES(?,?,?)";
+        
+        try {
+            PreparedStatement pst = conecta.conn.prepareStatement(sql);
+            pst.setString(1, null);
+            pst.setString(2, backlog.getDataDeCriacao().toString());
+            pst.setString(3, backlog.getDataUltimaModificacao().toString());
+            pst.execute();
+            JOptionPane.showMessageDialog(null, "Cadastrado com sucesso!");
+        } catch (SQLException ex) {
+            Logger.getLogger(BacklogControl.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, "Erro ao Conectar! \n Erro: " + ex.getMessage());
+        }
+        conecta.desconecta();
+        return true;
     }
     
     public void deletarBacklog(){
