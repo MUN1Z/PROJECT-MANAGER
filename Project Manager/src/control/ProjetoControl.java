e58e3943c08bd717b60a84b7d580d54a929e00e7
raiffsantos <raiff_santos@hotmail.com>
2016-06-03 23:34:21 -0300

    Documentando o código e criação do método editarProjeto

--- Project Manager/src/control/ProjetoControl.java
+++ Project Manager/src/control/ProjetoControl.java
@@ -44,9 +44,7 @@ public class ProjetoControl {
         ConectaDB conecta = new ConectaDB();
         conecta.conexao();
         String sql = "INSERT INTO projeto() VALUES(?,?,?,?)";
-        
         try {
-            
             PreparedStatement pst = conecta.conn.prepareStatement(sql);
             pst.setString(1, null);
             pst.setString(2, projeto.getDescricaoGeral());
@@ -62,12 +60,17 @@ public class ProjetoControl {
         return true;
     }
     
+    /**
+     * Método que recebe um objeto do tipo Projeto e deleta o objeto do Banco de
+     * dados atraves do id
+     * @author Raiff
+     * @param projeto
+     * @return boolean
+     */
     public static boolean deletarProjeto(Projeto projeto){
-        
         ConectaDB conecta = new ConectaDB();
         conecta.conexao();
         String sql = "DELETE FROM projeto WHERE id = ?";
-        
         try {
             PreparedStatement pst = conecta.conn.prepareStatement(sql);
             pst.setLong(1, projeto.getId());
@@ -76,31 +79,51 @@ public class ProjetoControl {
             Logger.getLogger(ProjetoControl.class.getName()).log(Level.SEVERE, null, ex);
             JOptionPane.showMessageDialog(null, "Erro ao Conectar! \n Erro: " + ex.getMessage());
         }
-        
-        
         return true;
     }
     
-    public static boolean editarProjeto(){
+    /**
+     * Método que recebe um objeto do tipo projeto e faz a atualização dos dados
+     * do objeto no banco de dados.
+     * @author Raiff
+     * @param projeto
+     * @return boolean
+     */
+    public static boolean editarProjeto(Projeto projeto){
+        ConectaDB conecta = new ConectaDB();
+        conecta.conexao();
+        String sql = "UPDATE projeto SET descricao = ?, data_criacao = ?, data_final = ?";
+        try {
+            PreparedStatement pst = conecta.conn.prepareStatement(sql);
+            pst.setString(1, projeto.getDescricaoGeral());
+            pst.setString(2, projeto.getDataDeCriacao());
+            pst.setString(3, projeto.getDataDeFinalizacao());
+            pst.execute();
+        } catch (SQLException ex) {
+            Logger.getLogger(ProjetoControl.class.getName()).log(Level.SEVERE, null, ex);
+        }
         return true;
     }
     
+    /**
+     * Método que seleciona todos os atributos da tabela projeto do banco de
+     * dados e insere as informações em um ArrayList de objetos do tipo Projeto
+     * @author Raiff
+     * @return ArrayList
+     */
     public static ArrayList listarProjeto(){
         ConectaDB conecta = new ConectaDB();
         conecta.conexao();
-        
         try {
             ArrayList projeto = new ArrayList();
             String sql = "SELECT * FROM projeto";
-            
             PreparedStatement pst = conecta.conn.prepareStatement(sql);
             ResultSet rs = pst.executeQuery();
             while(rs.next()){
-                
                 projeto.add(new Object[]{ rs.getInt("id"),
-                    rs.getString("descricao"), 
-                    rs.getString("data_criacao"), 
-                    rs.getString("data_final")});
+                rs.getString("descricao"), 
+                rs.getString("data_criacao"), 
+                rs.getString("data_final")});
             }
             return projeto;
         } catch (SQLException ex) {
